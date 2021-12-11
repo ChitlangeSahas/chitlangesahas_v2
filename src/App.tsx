2cb6cf025b6ea27c3ad5351564d68424d941eb13
ChitlangeSahas <chitlangesahas@gmail.com>
2021-12-11 12:40:41 -0700

    feat: sidebar background white

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ function App() {
     <div>
       <Page>
         <Page.PageHeader />
-        {/*<Page.Contents>*/}
-        {/*  <h2>Page contents</h2>*/}
-        {/*</Page.Contents>*/}
-        {/*<Page.Footer>*/}
-        {/*  <h3>Page footer</h3>*/}
-        {/*</Page.Footer>*/}
+        <Page.Contents>
+          <h2>Page contents</h2>
+        </Page.Contents>
+        <Page.Footer>
+          <h3>Page footer</h3>
+        </Page.Footer>
       </Page>
     </div>
   )
